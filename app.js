f5eb2871b83d9daae01080dacfad91d284e737af
JoseBernardoSanchez <71847634+JoseBernardoSanchez@users.noreply.github.com>
2021-06-22 13:23:02 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+const buttonReset = document.getElementsByClassName('.btn_reset');
+const miss = 0;
